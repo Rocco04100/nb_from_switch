ee21060d3ff863add6ba6a4e1dffd6f006e28b88
rdemartino <rdemartino@massport.com>
2026-07-10 14:54:18 -0400

    ssh autodetect working

--- auto_discovery.py
+++ auto_discovery.py
@@ -1,41 +1,39 @@
 # Serial port /dev/ttyUSB0
-import serial
-from netmiko import ConnectHandler
 
-cisco_switch = {
-    "device_type": "cisco_ios_serial",
+from netmiko import ConnectHandler
+from netmiko.ssh_autodetect import SSHDetect
+
+switch = {
+    "device_type": "autodetect",
+    "host": "192.168.1.2",
     "username": "admin",
-    "password": "",
-    "secret": "",
-    "fast_cli": False,  # Critical: Disable fast CLI for serial
-    "conn_timeout": 30,  # Optional: Increase timeout for slow serial login
-    "serial_settings": {
-        "port": "/dev/ttyUSB0",
-        "baudrate": 9600,
-        "bytesize": serial.EIGHTBITS,
-        "parity": serial.PARITY_NONE,
-        "stopbits": serial.STOPBITS_ONE,
-    },
+    "password": "password1",
+    # "secret": "",
 }
 
-print("Connecting to console port...")
+print(f"Connecting to {switch['host']} ...")
 
 try:
-    net_connect = ConnectHandler(**cisco_switch)
+    guesser = SSHDetect(**switch)
+    best_match = guesser.autodetect()
 
-    v_output = net_connect.send_command("show version", read_timeout=30)
+    if not best_match:
+        raise Exception("Auto-detection failed. Could not identify device OS.")
 
-    print("\n--- Show Version Output ---\n")
-    print(v_output)
+    print(f"✅ Detected OS: {best_match}")
+    # net_connect = ConnectHandler(**cisco_switch)
 
-    m_output = net_connect.send_command("show mac address-table", read_timeout=30)
-    print("\n--- Show mac Output ---\n")
-    print(m_output)
+    # v_output = net_connect.send_command("show version", read_timeout=30)
 
-    net_connect.disconnect()
-    print("\nConnection closed.")
+    # print("\n--- Show Version Output ---\n")
+    # print(v_output)
+
+    # m_output = net_connect.send_command("show mac address-table", read_timeout=30)
+    # print("\n--- Show mac Output ---\n")
+    # print(m_output)
+
+    # net_connect.disconnect()
+    # print("\nConnection closed.")
 
 except Exception as e:
     print(f"\nConnection failed: {e}")
-
-print("hello")
