41dddf39bafa06eb4b4b1c2c3abc167882eab68c
rdemartino <rdemartino@massport.com>
2026-07-13 10:35:09 -0400

    show commands printing to console parsed by textfsm

--- auto_discovery.py
+++ auto_discovery.py
@@ -3,37 +3,79 @@
 from netmiko import ConnectHandler
 from netmiko.ssh_autodetect import SSHDetect
 
+# from netmiko.ssh_exception import (
+#     NetmikoAuthenticationException,
+#     NetmikoTimeoutException,
+# )
+
 switch = {
     "device_type": "autodetect",
     "host": "192.168.1.2",
     "username": "admin",
     "password": "password1",
     # "secret": "",
+    "conn_timeout": 15,
+    "auth_timeout": 15,
+    "global_delay_factor": 2,
 }
 
-print(f"Connecting to {switch['host']} ...")
+print(f"Connecting to {switch['host']}...")
 
-try:
-    guesser = SSHDetect(**switch)
-    best_match = guesser.autodetect()
 
-    if not best_match:
-        raise Exception("Auto-detection failed. Could not identify device OS.")
+def detect_os():
+    try:
+        guesser = SSHDetect(**switch)
+        best_match = guesser.autodetect()
 
-    print(f"✅ Detected OS: {best_match}")
-    # net_connect = ConnectHandler(**cisco_switch)
+        if not best_match:
+            raise Exception("Auto-detection failed. Could not identify device OS.")
 
-    # v_output = net_connect.send_command("show version", read_timeout=30)
+        print(f"✅ Detected OS: {best_match}")
+        return best_match
 
-    # print("\n--- Show Version Output ---\n")
-    # print(v_output)
+    except Exception as e:
+        print(f"\nConnection failed: {e}")
+    return None
 
-    # m_output = net_connect.send_command("show mac address-table", read_timeout=30)
-    # print("\n--- Show mac Output ---\n")
-    # print(m_output)
 
-    # net_connect.disconnect()
-    # print("\nConnection closed.")
+def send_command(os_type, commands: list):
 
-except Exception as e:
-    print(f"\nConnection failed: {e}")
+    net_connect = None
+    if os_type is None:
+        print("Operating Sytem not detected aborting...")
+        return
+
+    switch["device_type"] = os_type
+    net_connect = ConnectHandler(**switch)
+
+    if isinstance(commands, str):  # convert single string command to list
+        commands = [commands]
+
+    for command in commands:
+        try:
+            output = net_connect.send_command(
+                command, read_timeout=30, use_textfsm=True
+            )
+            if isinstance(output, list):
+                print(f"\n--- {command} (Parsed) ---")
+                for row in output:
+                    print(row)
+            else:
+                print(f"\n--- {command} (Raw) ---")
+                print(output)
+
+        # except (NetmikoTimeoutException, NetmikoAuthenticationException) as e:
+        #     print(f"❌ Connection Error: {e}")
+        except Exception as e:
+            print(
+                f"Command Error(usually wrong output due to syntax error in command): {e}"
+            )
+
+    if net_connect:
+        net_connect.disconnect()
+        print("\nConnection closed.")
+
+
+detected_os = "cisco_ios"
+commands = ["show ip interface brief", "show interface status"]
+send_command(detected_os, commands)
